9bf3e326ed880303c3cf2b2844d51eada9c9403c
Pablo Zagnoli <68761353+pablozagnoli@users.noreply.github.com>
2021-03-01 23:16:36 -0300

    Add files via upload
    Calculadora das 4 operações Primarias em c# orientado a Objeto.
    Para rodar a aplicação, Realizar o Download do DotNet em sua  maquina.
    Após download do DotNet, ir para a pasta o programa, abri o PowerShell ou CMD na pasta, e digitar o comando "DOTNET run"

---
+++ Calculadora/Calculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    class Calculo
+    {
+        public int N1;
+        public int N2;
+        
+        public int Adicao()
+        {
+            return  N1 + N2;
+        }
+        public int Subtracao()
+        {
+            return N1 - N2;
+        }
+        public int Multiplicacao()
+        {
+            return N1 * N2;
+        }
+        public int Divisao()
+        {
+            return N1 - N2;
+        }
+
+    }
+}
+
---
+++ Calculadora/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Bem Vindo A Calculadora Orientada A obejto :)");
+            Calculo C = new Calculo();
+
+            Console.WriteLine("Digite o Primeiro Numero");
+            C.N1 = int.Parse( Console.ReadLine());
+            Console.WriteLine("Digite o Segundo Numero");
+            C.N2 = int.Parse( Console.ReadLine());
+            
+
+            int R = C.Adicao();
+            int R0 = C.Subtracao();
+            int R1 = C.Multiplicacao();
+            int R2 = C.Divisao();
+
+            Console.WriteLine("O resultado da Adição e:" + R );
+            Console.WriteLine("O resultado da Subtração e:" + R0);
+            Console.WriteLine("O resultado da Multiplicação e:" + R1);
+            Console.WriteLine("O resultado da Divisão e:" + R2);
+
+
+        }
+    }
+}
